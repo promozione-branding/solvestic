c7b20fca28c0f521b199f88a3e75f245b4548c6f
manish kumar <mk7219630@gmail.com>
2026-09-01 11:58:11 +0530

    new code added

--- app/layout.js
+++ app/layout.js
@@ -14,7 +14,8 @@ const geistMono = Geist_Mono({
 
 export const metadata = {
   title: "Solvestic | Science-Backed Skincare for Indian Skin",
-  description: "Discover Solvestic skincare, thoughtfully formulated for Indian skin to target pigmentation, acne, dryness, pores and dullness with effective solutions.",
+  description:
+    "Discover Solvestic skincare, thoughtfully formulated for Indian skin to target pigmentation, acne, dryness, pores and dullness with effective solutions.",
 };
 
 export default function RootLayout({ children }) {
@@ -37,11 +38,47 @@ export default function RootLayout({ children }) {
           `}
         </Script>
 
+        {/* Meta Pixel Code */}
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;
+            n.push=n;
+            n.loaded=!0;
+            n.version='2.0';
+            n.queue=[];
+            t=b.createElement(e);
+            t.async=!0;
+            t.src=v;
+            s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)
+            }(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+
+            fbq('init', '28198039089828210');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+
         <style>
-          @import url('https://fonts.googleapis.com/css2?family=Antonio:wght@100..700&family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Momo+Trust+Display&family=Outfit:wght@360&family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&family=Poppins:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Saira+Stencil:wdth,wght@125,700&display=swap');
+          @import
+          url('https://fonts.googleapis.com/css2?family=Antonio:wght@100..700&family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Momo+Trust+Display&family=Outfit:wght@360&family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&family=Poppins:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Saira+Stencil:wdth,wght@125,700&display=swap');
         </style>
       </head>
-      <body className="min-h-full flex flex-col outfit-dmsans">{children}</body>
+      <body className="min-h-full flex flex-col outfit-dmsans">
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=28198039089828210&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
+        {children}
+      </body>
     </html>
   );
 }
